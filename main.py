c9da3083bdcaeadae0a1e41b445f507c848d890d
Arnau kmpru <149146774+Dracoketh134@users.noreply.github.com>
2025-06-05 18:28:30 +0200

    He creat el esquelet del main

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-#hola
+import pygame
+
+pygame.init()
+
+
+amplada_pantalla = 800
+altura_pantalla = 600
+pantalla = pygame.display.set_mode(amplada_pantalla, altura_pantalla)
+pygame.display.set_caption("Minigames")
+
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pantalla.fill("blue")
+
+    
+    pygame.display.update()
+    clock.tick(60)
+
+pygame.quit()
