24372c69be1a727ebd9febd5f753faa8b358517c
Richi <132059486+Napalman7393@users.noreply.github.com>
2025-06-12 18:13:02 +0200

    Update ReactionTime.py

--- main_screen/src/Classes/Reactiontime/ReactionTime.py
+++ main_screen/src/Classes/Reactiontime/ReactionTime.py
@@ -3,7 +3,6 @@ pygame.init()
 import random
 class ReactionTimeGame:
     def __init__(self, time,):
-      
         self.time = time
 
 
@@ -15,12 +14,14 @@ pygame.display.set_caption("Reaction Time Game")
 
 #ja es la variable que indica si es el momento de clicar o no
 Ja = False
-
+joc = ReactionTimeGame(time=random.randint(1, 30))
+pygame.time.get_ticks()
 runin = True
 while runin:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             runin = False
+
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             Ja  = True
