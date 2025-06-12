9c7ec5c0ccd1ca624f1da56dc346f40f975a6a13
Richi <132059486+Napalman7393@users.noreply.github.com>
2025-06-12 18:23:52 +0200

    Update ReactionTime.py

--- main_screen/src/Classes/Reactiontime/ReactionTime.py
+++ main_screen/src/Classes/Reactiontime/ReactionTime.py
@@ -1,41 +1,34 @@
 import pygame
-pygame.init()
 import random
+
 class ReactionTimeGame:
-    def __init__(self,):
-        self.time = random.randint(1, 5) * 1000 
+    def __init__(self):
+        self.time = random.randint(1, 5) * 1000
+        self.amplada_pantalla = 1270
+        self.altura_pantalla = 720
+        self.pantalla = pygame.display.set_mode((self.amplada_pantalla, self.altura_pantalla))
+        pygame.display.set_caption("Reaction Time Game")
+        self.fish = pygame.image.load("main_screen/images/Fish.png")
+        self.ja = False
 
+    def run(self):
+        runin = True
+        while runin:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    runin = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    self.ja = True
 
-    amplada_pantalla = 1270
-    altura_pantalla = 720
-    pantalla = pygame.display.set_mode((amplada_pantalla, altura_pantalla))
-    pygame.display.set_caption("Reaction Time Game")
-    fish = pygame.image.load("main_screen/images/Fish.png")
+            if not self.ja:
+                self.pantalla.fill("Red")
+            else:
+                self.pantalla.fill("Green")
 
-    #ja es la variable que indica si es el momento de clicar o no
-    Ja = False
-    pygame.time.get_ticks()
-    runin = True
-    while runin:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                runin = False
+            pygame.display.update()
+        pygame.quit()
 
-
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                Ja  = True
-
-        if not Ja:
-            pantalla.fill("Red")
-        else:
-            pantalla.fill("Green")
-
-
-        
-
-
-
-        pygame.display.update()
-
-
-    pygame.quit()
+# Ara pots utilitzar la classe dins de main:
+if __name__ == "__main__":
+    joc = ReactionTimeGame()
+    joc.run()
