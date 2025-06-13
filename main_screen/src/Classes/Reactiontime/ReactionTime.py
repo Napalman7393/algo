0490cbb757d5381260c0d86ee0da209503dde12e
Arnau kmpru <149146774+Dracoketh134@users.noreply.github.com>
2025-06-13 11:49:41 +0200

    canvis en el fitxer de buttons

--- main_screen/src/Classes/Reactiontime/ReactionTime.py
+++ main_screen/src/Classes/Reactiontime/ReactionTime.py
@@ -3,23 +3,28 @@ import random
 
 class ReactionTimeGame:
     def __init__(self):
+
         self.amplada_pantalla = 1270
         self.altura_pantalla = 720
         self.pantalla = pygame.display.set_mode((self.amplada_pantalla, self.altura_pantalla))
         pygame.display.set_caption("Reaction Time Game")
+
         self.fish = pygame.image.load("main_screen/images/Fish.png")
         self.ja = False
         self.temps_espera = random.randint(1, 5) * 1000
         self.inici = pygame.time.get_ticks()
         self.score = None
         self.panSco = False
+        # PanSco és una variable per controlar si s'ha de mostrar el text del temps de reacció a la pantalla o no.
 
     def run(self):
         runin = True
         while runin:
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     runin = False
+                    
                 if self.ja and event.type == pygame.MOUSEBUTTONDOWN:
                     self.score = pygame.time.get_ticks() - (self.inici + self.temps_espera)
                     self.font = pygame.font.SysFont(None, 74)
--- main_screen/src/buttons.py
+++ main_screen/src/buttons.py
@@ -7,21 +7,30 @@ altura_pantalla = 720
 
 play_image = pygame.image.load("main_screen/images/Buttons/Play_button.png")
 plat_apretat_image = pygame.image.load("main_screen/images/Buttons/Play_button_apretat.png")
+
 difficulty_button_easy_image = pygame.image.load("main_screen/images/Buttons/Difficulty_easy_button.png")
 difficulty_button_medium_image = pygame.image.load("main_screen/images/Buttons/Difficulty_medium_button.png")
 difficulty_button_hard_image = pygame.image.load("main_screen/images/Buttons/Difficulty_hard_button.png")
 
+reaction_time_image = pygame.image.load("main_screen/images/Buttons/Reaction_time_button.png")
+
 # Escalatge de les imatges
 
-play_button = pygame.transform.scale(play_image, ((play_image.get_width() * 9), (play_image.get_width() * 3)))
-play_button_apretat = pygame.transform.scale(plat_apretat_image, ((plat_apretat_image.get_width() * 9), (plat_apretat_image.get_width() * 3)))
+play_button = pygame.transform.scale(plat_apretat_image, ((plat_apretat_image.get_width() * 9), (plat_apretat_image.get_width() * 3)))
+play_button_apretat = pygame.transform.scale(play_image, ((play_image.get_width() * 9), (play_image.get_width() * 3)))
+
 difficulty_button_easy = pygame.transform.scale(difficulty_button_easy_image, ((difficulty_button_easy_image.get_width() * 9), ((pygame.image.load("main_screen/images/Buttons/Play_button.png")).get_width() * 3)))
 difficulty_button_medium = pygame.transform.scale(difficulty_button_medium_image, ((difficulty_button_medium_image.get_width() * 9), (difficulty_button_medium_image.get_width() * 3)))
 difficulty_button_hard = pygame.transform.scale(difficulty_button_hard_image, ((difficulty_button_hard_image.get_width() * 9), (difficulty_button_hard_image.get_width() * 3)))
 
+reaction_time_button = pygame.transform.scale(reaction_time_image, ((reaction_time_image.get_width() * 9), (reaction_time_image.get_width() * 3)))
+
 # Agafar els rects de les imatges
-play_button_rect = play_button.get_rect(center = (amplada_pantalla // 2, altura_pantalla // 2))
-play_button_apretat_rect = play_button_apretat.get_rect(center = (amplada_pantalla // 2, altura_pantalla // 2))
+play_button_rect = play_button_apretat.get_rect(center = (amplada_pantalla // 2, altura_pantalla // 2))
+play_button_apretat_rect = play_button.get_rect(center = (amplada_pantalla // 2, altura_pantalla // 2))
+
 difficulty_button_easy_rect = difficulty_button_easy.get_rect(topleft = (50, 50))
 difficulty_button_medium_rect = difficulty_button_medium.get_rect(topleft = (50, 150))
 difficulty_button_hard_rect = difficulty_button_hard.get_rect(topleft = (50, 250))
+
+reaction_time_button_rect = reaction_time_button.get_rect(topleft = (50, 350))
